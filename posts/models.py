2f6de164e217d6e79ee3c298c78d3fe8f50d296e
DGK <rlaehdrb0923@gmail.com>
2022-07-20 12:30:32 +0900

    feat: 프로젝트 DB 모델링 (#2)

---
+++ posts/models.py
@@ -0,0 +1,34 @@
+from django.db   import models
+
+from core.models import TimeStampModel
+
+
+class Post(TimeStampModel):
+    
+    STATUS_TYPES = [
+        ('in_posting', 'Post in posting'),
+        ('deleted',    'Post deleted'),
+    ]
+    
+    users   = models.ForeignKey('users.User', on_delete=models.CASCADE)
+    tags    = models.ForeignKey('Tag', on_delete=models.CASCADE)
+    content = models.TextField()
+    title   = models.CharField(max_length=100)
+    views   = models.PositiveIntegerField(default=0)
+    status  = models.CharField(max_length=200, choices=STATUS_TYPES, default='in_posting')
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        db_table = 'posts'
+        
+
+class Tag(TimeStampModel):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'tags'    
