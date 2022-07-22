f23f7d6b6fa3b65bc2fb1787d8eb3b4f7cd1339e
DGK <rlaehdrb0923@gmail.com>
2022-07-22 12:51:59 +0900

    feat: 게시물 생성 기능 테스트코드 작성 (#15)

---
+++ posts/tests/tests_post_update.py
@@ -0,0 +1,10 @@
+import json
+
+from rest_framework.test import APITestCase, APIClient
+
+from users.models import User
+from posts.models import Post
+
+
+class PostUpdateTest(APITestCase):
+    pass
